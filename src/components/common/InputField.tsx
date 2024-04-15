e0e1ba65c9f5f9d3a6e433c298a1903ee4317f2d
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-04-16 00:06:54 +0900

    style: 라벨드 인풋 스타일 적용

--- src/components/common/InputField.tsx
+++ src/components/common/InputField.tsx
@@ -1,3 +1,50 @@
+import styled from 'styled-components';
+import theme from '@/styles/theme';
+
+const onMobile = `@media only screen and (max-width: 375px)`;
+
+const S = {
+  Layout: styled.div`
+    display: inline-flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 0.8rem;
+  `,
+  Label: styled.label`
+    color: ${theme.color.black};
+    font-family: Pretendard;
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  `,
+  Input: styled.input`
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    width: 52rem;
+    height: 5rem;
+    padding: 1.5rem 1.6rem;
+    border-radius: 0.8rem;
+    border: 1px solid ${theme.color.grayLight};
+    background: ${theme.color.white};
+
+    ${onMobile} {
+      width: 28.7rem;
+      height: 4.2rem;
+    }
+
+    &::placeholder {
+      color: ${theme.color.gray};
+      font-family: Pretendard;
+      font-size: 1.6rem;
+      font-style: normal;
+      font-weight: 400;
+      line-height: normal;
+    }
+  `,
+};
+
 interface InputFieldProps {
   label: string;
   id: string;
