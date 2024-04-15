abc01e6d26df6dca14de920750ba5965a9345446
hyunjun9788 <namhyunjun9788@gmail.com>
2024-04-16 00:21:33 +0900

    feat: 프로필 박스 영역 구현

--- src/components/common/DashBoardHeader.tsx
+++ src/components/common/DashBoardHeader.tsx
@@ -1,25 +1,58 @@
+import Image from 'next/image';
 import styled from 'styled-components';
+import ProfileImg from '@/public/icon/profileImg.svg';
 import theme from '@/styles/theme';
 
 interface HeaderProps {
   menuName: string;
+  profileName: string;
+  profileImgURL: string;
 }
 const S = {
   Header: styled.nav`
     display: flex;
+    justify-content: space-between;
     align-items: center;
     height: 7rem;
     border-bottom: 0.1rem solid ${theme.color.grayLight};
   `,
+  MenuNameAndButtonBox: styled.div`
+    display: flex;
+    flex: 1;
+    justify-content: space-between;
+  `,
   MenuName: styled.div`
+    margin-left: 30rem;
     font-weight: 700;
+  `,
+
+  ProfileBox: styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0 40px;
+    border-left: 1px solid ${theme.color.grayLight};
+    gap: 1rem;
+  `,
+  ProfileName: styled.div`
+    font-weight: 500;
   `,
 };
 
-function DashBoardHeader({ menuName }: HeaderProps) {
+function DashBoardHeader({
+  menuName,
+  profileName,
+  profileImgURL,
+}: HeaderProps) {
   return (
     <S.Header>
-      <S.MenuName>{menuName}</S.MenuName>
+      <S.MenuNameAndButtonBox>
+        <S.MenuName>{menuName}</S.MenuName>
+      </S.MenuNameAndButtonBox>
+      <S.ProfileBox>
+        {/* <Image src={profileImgURL} width={38} height={38} alt="profileImg" /> */}
+        <S.ProfileName>{profileName}</S.ProfileName>
+      </S.ProfileBox>
     </S.Header>
   );
 }
--- src/pages/hj.tsx
+++ src/pages/hj.tsx
@@ -1,15 +1,21 @@
 import DashBoardHeader from '@/components/common/DashBoardHeader';
 import HashTag from '@/components/common/tag/HashTag';
 import StateTag from '@/components/common/tag/StateTag';
+import ProfileImg from '@/public/icon/profileImg.svg';
 import '@/styles/GlobalStyle';
 
 function hj() {
   const stateTags = ['To Do', 'On Progress', 'Done'];
   const isMobile = false;
   const hashTag = ['프로젝트', '프론트엔드', '어려워'];
+  const IMAGE_URL = 'https://i.ibb.co/ysRQMyj/me.jpg';
   return (
     <>
-      <DashBoardHeader menuName={'내 대시보드'} />
+      <DashBoardHeader
+        menuName={'내 대시보드'}
+        profileName={'배유철'}
+        profileImgURL={IMAGE_URL}
+      />
       <div>
         {stateTags.map((tag, index) => (
           <StateTag isMobile={isMobile}>{tag}</StateTag>
