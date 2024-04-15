4d91de1ec8a96e809439a222246d568b05e2a655
박지윤 <tkrhkfld123@naver.com>
2024-04-15 17:37:22 +0900

    chore: props 추가

--- src/pages/jy.tsx
+++ src/pages/jy.tsx
@@ -3,7 +3,7 @@ import ImgFileUpload from '@/components/common/ImgFileUpload';
 function jy() {
   return (
     <div>
-      <ImgFileUpload title="이미지" edit={true} />
+      <ImgFileUpload title="이미지" edit={false} small={true} />
     </div>
   );
 }
