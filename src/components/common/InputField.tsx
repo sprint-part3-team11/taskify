825214f9b57e47e65161e859b4894fb091338e88
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-04-16 05:49:13 +0900

    style: mobile 기준으로 수정 & 용도에 따른 인풋 스타일 분리

--- src/components/common/InputField.tsx
+++ src/components/common/InputField.tsx
@@ -1,52 +1,61 @@
 import styled from 'styled-components';
-import theme from '@/styles/theme';
 
-const onMobile = `@media only screen and (max-width: 375px)`;
+const BREAKPOINT = {
+  medium: '376px',
+  large: '744px',
+};
+
+const onPC = `@media only screen and (min-width: ${BREAKPOINT.medium})`;
 
 const S = {
   Layout: styled.div`
-    display: inline-flex;
+    display: flex;
     flex-direction: column;
     align-items: flex-start;
     gap: 0.8rem;
   `,
   Label: styled.label`
-    color: ${theme.color.black};
-    font-family: Pretendard;
+    color: ${({ theme }) => theme.color.black};
     font-size: 1.6rem;
-    font-style: normal;
     font-weight: 400;
-    line-height: normal;
   `,
-  Input: styled.input`
+  Input: styled.input<{
+    usage: 'form' | 'modal';
+  }>`
     display: flex;
     align-items: center;
     gap: 1rem;
-    width: 52rem;
+    width: 35.1rem;
     height: 5rem;
     padding: 1.5rem 1.6rem;
     border-radius: 0.8rem;
-    border: 1px solid ${theme.color.grayLight};
-    background: ${theme.color.white};
+    border: 1px solid ${({ theme }) => theme.color.grayLight};
+    background: ${({ theme }) => theme.color.white};
 
-    ${onMobile} {
-      width: 28.7rem;
-      height: 4.2rem;
+    ${onPC} {
+      ${({ usage }) =>
+        usage === 'form'
+          ? `
+              width: 52rem;
+              height: 5rem;
+            `
+          : `
+              width: 48.4rem;
+              height: 4.8rem;
+            `}
     }
 
     &::placeholder {
-      color: ${theme.color.gray};
-      font-family: Pretendard;
+      color: ${({ theme }) => theme.color.gray};
       font-size: 1.6rem;
-      font-style: normal;
       font-weight: 400;
-      line-height: normal;
     }
   `,
 };
 
 interface InputFieldProps {
   label: string;
+  usage: 'form' | 'modal';
   id: string;
   type: string;
   placeholder: string;
@@ -62,3 +71,12 @@ function InputField({ label, id, ...rest }: InputFieldProps) {
 }
 
 export default InputField;
+/**
+ * @component
+ * @param label - label 이름 (이메일, 제목 ...)
+ * @param usage - 해당 Input 컴포넌트가 form에서 사용되는지, modal에서 사용되는지
+ * - 사용되는 곳에 따라서 사이즈가 다름
+ * @param id - label, input 연결용 id (input, title ...)
+ * @param type - input 타입 (email, text ...)
+ * @param placeholder - placeholder
+ */
