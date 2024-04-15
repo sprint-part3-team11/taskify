dfdb816043e93ff52acda18e08ff4c32c8cf835d
sihyonn <sihyeon982@naver.com>
2024-04-15 22:55:19 +0900

    feat: Button 컴포넌트 생성 및 사용 테스트

--- src/pages/sh.tsx
+++ src/pages/sh.tsx
@@ -1,5 +1,30 @@
+import Button from '@/components/common/button/Button';
+import { BUTTON_TYPE } from '@/constants/BUTTON_TYPE';
+
 function sh() {
-  return <div>시현 테스트페이지</div>;
+  return (
+    <>
+      <Button size="S" styleType={BUTTON_TYPE.DESTRUCTIVE}>
+        삭제
+      </Button>
+      <Button
+        size="L"
+        styleType={BUTTON_TYPE.PRIMARY}
+        style={{ width: '30rem' }}
+        disabled
+      >
+        버튼
+      </Button>
+      <Button
+        size="L"
+        styleType={BUTTON_TYPE.PRIMARY}
+        style={{ width: '30rem' }}
+      >
+        버튼
+      </Button>
+      <Button styleType={BUTTON_TYPE.SECONDARY}>취소</Button>
+    </>
+  );
 }
 
 export default sh;
