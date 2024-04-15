ba96f64cd04a83ea8decbe51670c867f5b4892c3
MinJoon_Choi <minjoon1998@gmail.com>
2024-04-15 17:36:26 +0900

    style: styled-components로 Modal 뒷배경, 컨텐츠 스타일 지정

---
+++ src/components/common/Modal/BackDropModal.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+const S = {
+  ModalBackground: styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 999;
+  `,
+  ModalContent: styled.div`
+    padding: 20px;
+    background: ${({ theme }) => theme.color.white};
+    border-radius: 8px;
+    z-index: 1000;
+  `,
+};
