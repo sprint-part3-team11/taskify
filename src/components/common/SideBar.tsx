a457d5496b0c309893d46bdc1f13e6976701e966
박지윤 <tkrhkfld123@naver.com>
2024-04-17 01:14:23 +0900

    style: 사이드바 스타일 적용

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 import CircleColor from './CircleColor';
 import styled from 'styled-components';
+import MEDIA_QUERIES from '@/constants/MEDIAQUERIES';
 import AddDashBoardIcon from '@/public/icon/addDashboardBox.svg';
 import CreateByMe from '@/public/icon/creatByMe.svg';
 import LogoIcon from '@/public/icon/logo.svg';
@@ -14,10 +15,27 @@ const S = {
     padding: 1.25rem 1.5rem;
     border: 1px solid ${({ theme }) => theme.color.grayLight};
     background-color: ${({ theme }) => theme.color.white};
+
+    ${MEDIA_QUERIES.onTablet} {
+      width: 15rem;
+    }
+
+    ${MEDIA_QUERIES.onMobile} {
+      width: 7rem;
+    }
   `,
   Logo: styled.div`
     margin-bottom: 3.75rem;
     cursor: pointer;
+
+    ${MEDIA_QUERIES.onMobile} {
+      margin-left: 0.625rem;
+    }
+  `,
+  LogoText: styled(LogoText)`
+    ${MEDIA_QUERIES.onMobile} {
+      display: none;
+    }
   `,
   AddDashBoard: styled.div`
     display: flex;
@@ -26,19 +44,44 @@ const S = {
   `,
   AddText: styled.span`
     padding-top: 3%;
-    color: ${({ theme }) => theme.color.grayDark};
+    color: ${({ theme }) => theme.color.main};
     font-size: 1rem;
     font-weight: 700;
     margin-right: 9.5rem;
+
+    ${MEDIA_QUERIES.onTablet} {
+      margin-right: 3.5rem;
+    }
+
+    ${MEDIA_QUERIES.onMobile} {
+      display: none;
+    }
   `,
   AddDashBoardIcon: styled(AddDashBoardIcon)`
     cursor: pointer;
+
+    ${MEDIA_QUERIES.onMobile} {
+      margin-left: 0.7rem;
+    }
   `,
   DashboardItemWrapper: styled.div`
     display: flex;
     align-items: center;
     padding: 10px;
+    margin-top: 0.85rem;
+
+    &:hover {
+      background: var(--violet-violet-8, #f1effd);
+      border-radius: 0.55rem;
+    }
+
+    ${MEDIA_QUERIES.onMobile} {
+      padding: 1.25rem;
+      margin-top: 0.85rem;
+      margin-left: 0.2rem;
+    }
   `,
+  CircleColor: styled(CircleColor)``,
   DashboardItem: styled.li<{ active: boolean }>`
     cursor: pointer;
     color: ${({ theme }) => theme.color.grayDark};
@@ -46,6 +89,10 @@ const S = {
     font-weight: 500;
     ${({ active }) => active && `font-weight: bold;`}
     margin-left: 1.325rem;
+
+    ${MEDIA_QUERIES.onMobile} {
+      display: none;
+    }
   `,
 };
 
@@ -77,7 +124,7 @@ const Sidebar: React.FC<SidebarProps> = ({ dashboards }) => {
     <S.SidebarWrapper>
       <S.Logo onClick={handleLogoClick}>
         <LogoIcon />
-        <LogoText />
+        <S.LogoText />
       </S.Logo>
       <S.AddDashBoard>
         <S.AddText>Dash Boards</S.AddText>
@@ -86,7 +133,7 @@ const Sidebar: React.FC<SidebarProps> = ({ dashboards }) => {
       <ul>
         {updatedDashboards.map((dashboard) => (
           <S.DashboardItemWrapper key={dashboard.id}>
-            <CircleColor color={dashboard.color} />
+            <S.CircleColor color={dashboard.color} />
             <S.DashboardItem
               active={dashboard.id === router.query.dashboardId}
               onClick={() => handleDashboardClick(dashboard.id)}
@@ -96,7 +143,7 @@ const Sidebar: React.FC<SidebarProps> = ({ dashboards }) => {
           </S.DashboardItemWrapper>
         ))}
       </ul>
-      {/* 모달 추가 */}
+      {/* 여기에 모달 추가 */}
     </S.SidebarWrapper>
   );
 };
