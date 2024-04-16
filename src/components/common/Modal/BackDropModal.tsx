b38343c643b770d65b2f6e4e5e4f1dc9768d445a
MinJoon_Choi <minjoon1998@gmail.com>
2024-04-17 03:14:36 +0900

    chore: jsDoc example 및 불필요한 코드 제거

--- src/components/common/Modal/BackDropModal.tsx
+++ src/components/common/Modal/BackDropModal.tsx
@@ -2,21 +2,10 @@ import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import styled, { keyframes } from 'styled-components';
 
-const unfoldIn = keyframes`
-  0% { transform: scaleY(.005) scaleX(0); }
-  50% { transform: scaleY(.005) scaleX(1); }
-  100% { transform: scaleY(1) scaleX(1); }
-`;
-
-const zoomIn = keyframes`
-  0% { transform: scale(0); }
-  100% { transform: scale(1); }
-`;
-
 const appearModal = keyframes`
-  0%{opacity:0}
-  75%{opacity:0}
-  100%{opacity:1}
+  0%{opacity:0.2;transform: scale(0.5);}
+
+  100%{opacity:1;transform: scale(1);}
 `;
 const S = {
   ModalBackground: styled.div`
@@ -30,15 +19,13 @@ const S = {
     width: 100%;
     height: 100%;
     background-color: rgba(0, 0, 0, 0.5);
-
-    animation: ${unfoldIn} 1s cubic-bezier(0.165, 0.84, 0.44, 1) forwards;
   `,
   ModalContainer: styled.div`
     padding: 3.2rem 2rem 2rem;
     background: ${({ theme }) => theme.color.white};
     border-radius: 0.8rem;
 
-    animation: ${appearModal} 1s;
+    animation: ${appearModal} 0.7s ease-in-out;
   `,
 };
 
@@ -54,48 +41,7 @@ interface BackDropModalProps {
  * @param isOpen - 모달이 열려있는지 확인
  * @param children - 모달 내부 내용
  * @param onClose - 모달을 닫을 때 사용할 함수
- * @example
- * 새 컬럼 생성 예시
- * import React, { useState } from 'react';
- * import BackDropModal from '@/components/common/Modal/BackDropModal';
  *
- * const CreateCategoryModal = () => {
- *   const [isModalOpen, setModalOpen] = useState(false);
- *   const openModal = () => setModalOpen(true);
- *   const closeModal = () => setModalOpen(false);
- *
- *   const [projectName, setProjectName] = useState("");
- *
- *   const handleInputChange = (e) => {
- *     setProjectName(e.target.value);
- *   };
- *
- *   const handleFormSubmit = (e) => {
- *    e.preventDefault(); // 페이지 리로드 방지
- *    // 카테고리 생성 로직 여기 적기
- *    closeModal(); // 모달 닫기
- *   };
- *
- *   return (
- *     <BackDropModal isOpen={isModalOpen} onClose={closeModal}>
- *       <h1>새 카테고리 생성</h1>
- *       <div>이름</div>
- *       <form onSubmit={handleFormSubmit}>
- *         <input
- *           type="text"
- *           placeholder="새로운 프로젝트"
- *           value={projectName}
- *           onChange={handleInputChange}
- *         />
- *       </form>
- *       <button onClick={closeModal}>취소</button>
- *       <button type="button" onClick={handleFormSubmit}>생성</button>
- *     </BackDropModal>
- *   );
- * };
- *
- * export default CreateCategoryModal;
- * );
  */
 
 function BackDropModal({ isOpen, children, onClose }: BackDropModalProps) {
