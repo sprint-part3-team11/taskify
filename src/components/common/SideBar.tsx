e5b4b0e4d48c1148872ac2f1dc932a06f6017d56
박지윤 <tkrhkfld123@naver.com>
2024-04-16 23:14:01 +0900

    style: 사이드바 구현

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -1,31 +1,59 @@
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import CircleColor from './CircleColor';
 import styled from 'styled-components';
+import AddDashBoardIcon from '@/public/icon/addDashboardBox.svg';
+import CreateByMe from '@/public/icon/creatByMe.svg';
+import LogoIcon from '@/public/icon/logo.svg';
+import LogoText from '@/public/icon/logoText.svg';
 
 const S = {
   SidebarWrapper: styled.div`
-    background-color: #f0f0f0;
-    width: 200px;
+    width: 20.75rem;
     height: 100vh;
-    padding: 20px;
+    padding: 1.25rem 1.5rem;
+    border: 1px solid ${({ theme }) => theme.color.grayLight};
+    background-color: ${({ theme }) => theme.color.white};
   `,
   Logo: styled.div`
-    font-size: 24px;
-    font-weight: bold;
-    margin-bottom: 20px;
+    margin-bottom: 3.75rem;
     cursor: pointer;
+  `,
+  AddDashBoard: styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 2.625rem;
+  `,
+  AddText: styled.span`
+    padding-top: 3%;
+    color: ${({ theme }) => theme.color.grayDark};
+    font-size: 1rem;
+    font-weight: 700;
+    margin-right: 9.5rem;
+  `,
+  AddDashBoardIcon: styled(AddDashBoardIcon)`
+    cursor: pointer;
+  `,
+  DashboardItemWrapper: styled.div`
+    display: flex;
+    align-items: center;
+    padding: 10px;
   `,
   DashboardItem: styled.li<{ active: boolean }>`
     cursor: pointer;
-    margin-bottom: 10px;
-    font-size: 16px;
+    color: ${({ theme }) => theme.color.grayDark};
+    font-size: 1.425rem;
+    font-weight: 500;
     ${({ active }) => active && `font-weight: bold;`}
+    margin-left: 1.325rem;
   `,
 };
 
 interface DashboardProps {
   id: string;
+  color: string;
   name: string;
+  createdByMe: boolean;
 }
 
 interface SidebarProps {
@@ -47,19 +75,28 @@ const Sidebar: React.FC<SidebarProps> = ({ dashboards }) => {
 
   return (
     <S.SidebarWrapper>
-      <S.Logo onClick={handleLogoClick}>로고</S.Logo>
+      <S.Logo onClick={handleLogoClick}>
+        <LogoIcon />
+        <LogoText />
+      </S.Logo>
+      <S.AddDashBoard>
+        <S.AddText>Dash Boards</S.AddText>
+        <S.AddDashBoardIcon />
+      </S.AddDashBoard>
       <ul>
         {updatedDashboards.map((dashboard) => (
-          <S.DashboardItem
-            key={dashboard.id}
-            active={dashboard.id === router.query.dashboardId}
-            onClick={() => handleDashboardClick(dashboard.id)}
-          >
-            {dashboard.name} {dashboard.id === router.query.dashboardId && '👑'}
-          </S.DashboardItem>
+          <S.DashboardItemWrapper key={dashboard.id}>
+            <CircleColor color={dashboard.color} />
+            <S.DashboardItem
+              active={dashboard.id === router.query.dashboardId}
+              onClick={() => handleDashboardClick(dashboard.id)}
+            >
+              {dashboard.name} {dashboard.createdByMe && <CreateByMe />}
+            </S.DashboardItem>
+          </S.DashboardItemWrapper>
         ))}
       </ul>
-      {/* 여기에 모달 추가  */}
+      {/* 모달 추가 */}
     </S.SidebarWrapper>
   );
 };
--- src/pages/jy.tsx
+++ src/pages/jy.tsx
@@ -12,9 +12,9 @@ const selectBoxOptions = [
 ];
 
 const dashboards = [
-  { id: '1', name: '대시보드 1' },
-  { id: '2', name: '대시보드 2' },
-  { id: '3', name: '대시보드 3' },
+  { id: '1', color: '#FFA500', name: '대시보드 1', createdByMe: true },
+  { id: '2', color: '#FF2660', name: '대시보드 2', createdByMe: true },
+  { id: '3', color: '#7AC555', name: '대시보드 3', createdByMe: false },
 ];
 function jy() {
   return (
