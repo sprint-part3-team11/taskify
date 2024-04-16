9bf3380a30d4ed6b9a604a0905ea45299eec181a
MinJoon_Choi <minjoon1998@gmail.com>
2024-04-17 03:06:56 +0900

    feat: 새 컬럼 추가 모달 input value prop으로 전달 로직 구현

--- src/components/common/Modal/AddNewColumnsModal.tsx
+++ src/components/common/Modal/AddNewColumnsModal.tsx
@@ -1,5 +1,7 @@
-import Button from '../button/Button';
+import { useState } from 'react';
 import styled from 'styled-components';
+import InputField from '@/components/common/InputField';
+import Button from '@/components/common/button/Button';
 import BackDropModal from '@/components/common/modal/BackDropModal';
 import MEDIA_QUERIES from '@/constants/MEDIAQUERIES';
 
@@ -17,21 +19,22 @@ const S = {
     font-size: 1.8rem;
     font-weight: 500;
   `,
-  Input: styled.input`
-    width: 48.4rem;
+  /* input 컴포넌트에서 스타일 변경하려면 어떻게 해야할까요..? 예를 들어 gap이나 font-size 조정하려구요. */
+  Input: styled(InputField)`
+    min-width: 48.4rem;
     padding: 1.5rem 1.6rem 1.4rem;
     margin-bottom: 2.8rem;
-    font-size: 1.6rem;
+
     border-radius: 0.6rem;
     border: 1px solid ${({ theme }) => theme.color.grayLight};
     background: ${({ theme }) => theme.color.white};
 
     color: ${({ theme }) => theme.color.body};
-    font-size: 1.6rem;
     font-weight: 400;
+    font-size: 1.6rem;
 
     ${MEDIA_QUERIES.onMobile} {
-      width: 29rem;
+      min-width: 29rem;
       margin-bottom: 2.4rem;
     }
   `,
@@ -54,19 +57,36 @@ const S = {
 interface AddNewColumnsModal {
   isOpen: boolean;
   onClose: () => void;
+  onCreate: (columnName: string) => void;
 }
 
-function AddNewColumnsModal({ isOpen, onClose }: AddNewColumnsModal) {
+function AddNewColumnsModal({ isOpen, onClose, onCreate }: AddNewColumnsModal) {
+  const [columnName, setColumnName] = useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setColumnName(e.target.value);
+  };
+
+  const handleCreate = () => {
+    onCreate(columnName);
+    onClose();
+  };
+
   return (
     <BackDropModal isOpen={isOpen} onClose={onClose}>
       <S.Title>새 컬럼 생성</S.Title>
-      <S.Text>이름</S.Text>
-      <S.Input placeholder="새로운 프로젝트" />
+      <S.Input
+        label="이름"
+        id="addNewColumn"
+        placeholder="새로운 프로젝트"
+        value={columnName}
+        onChange={handleInputChange}
+      />
       <S.ButtonContainer>
         <S.ImportButton onClick={onClose} styleType="SECONDARY" size="M">
           취소
         </S.ImportButton>
-        <S.ImportButton onClick={onClose} styleType="PRIMARY" size="M">
+        <S.ImportButton onClick={handleCreate} styleType="PRIMARY" size="M">
           생성
         </S.ImportButton>
       </S.ButtonContainer>
