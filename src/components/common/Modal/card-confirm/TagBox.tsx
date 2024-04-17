13a8a3cf2a2139e7fc29d096c9f6ea628a0378d6
hyunjun9788 <namhyunjun9788@gmail.com>
2024-04-17 17:50:27 +0900

    fix: 태그 정렬 깨짐 해결

--- src/components/common/Modal/card-confirm/TagBox.tsx
+++ src/components/common/Modal/card-confirm/TagBox.tsx
@@ -3,23 +3,28 @@ import StateTag from '../../tag/StateTag';
 import { CardConfirmModalProps } from './CardConfirmModal';
 import styled from 'styled-components';
 
-const HeightLine = styled.div`
-  border: 1px solid ${({ theme }) => theme.color.grayLight};
-  margin: 0 2rem 0.2rem 2rem;
-`;
+const S = {
+  TagBox: styled.div`
+    display: flex;
+  `,
 
+  HeightLine: styled.div`
+    border: 1px solid ${({ theme }) => theme.color.grayLight};
+    margin: 0 2rem 0.2rem 2rem;
+  `,
+};
 function TagBox({ cardInfoData }: CardConfirmModalProps) {
   return (
-    <>
+    <S.TagBox>
       <StateTag isMobile={false}>{cardInfoData.tags}</StateTag>
-      <HeightLine />
+      <S.HeightLine />
       <HashTag isMobile={false} index={0}>
         프로젝트
       </HashTag>
       <HashTag isMobile={false} index={1}>
         프론트엔드
       </HashTag>
-    </>
+    </S.TagBox>
   );
 }
 export default TagBox;
