8c2fb456b6b2ef46a91ab31fb28948dd6a27f29f
MinJoon_Choi <minjoon1998@gmail.com>
2024-04-18 02:29:25 +0900

    chore: export default로 변경(eslint)

--- src/constants/WARNING_MODAL_MESSAGES.ts
+++ src/constants/WARNING_MODAL_MESSAGES.ts
@@ -1,5 +1,7 @@
-export const WARNING_MODAL_MESSAGES = {
+const WARNING_MODAL_MESSAGES = {
   PASSWORD: '비밀번호가 일치하지 않습니다.',
   SUCCESS: '가입이 완료되었습니다!',
   ALREADY_USED: '이미 사용 중인 이메일입니다.',
 };
+
+export default WARNING_MODAL_MESSAGES;
