8b4164383ec7f6d1f3b35fa42275367aef4cc4c1
hyunjun9788 <namhyunjun9788@gmail.com>
2024-04-18 05:23:23 +0900

    style: 모달 사이드 박스 글자 크기, 패딩 세부 값 조정

--- src/components/common/Modal/card-confirm/SideBox.tsx
+++ src/components/common/Modal/card-confirm/SideBox.tsx
@@ -4,34 +4,58 @@ import styled from 'styled-components';
 
 const S = {
   SideContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
     width: 20rem;
     height: 15rem;
     border: 1px solid ${({ theme }) => theme.color.grayLight};
     border-radius: 1rem;
     margin-left: 2.3rem;
+    padding: 1.5rem 1.5rem 2.5rem 1.5rem;
   `,
 
+  AssigneeBox: styled.div`
+    display: flex;
+    flex-direction: column;
+  `,
+
+  Label: styled.div`
+    margin: 1rem 0;
+    font-size: 1.2rem;
+    font-weight: 600;
+  `,
   Assignee: styled.div`
     display: flex;
+    align-items: center;
+  `,
+
+  NickName: styled.div`
+    margin-left: 1rem;
+  `,
+  DueDate: styled.div`
+    font-size: 1.4rem;
   `,
 };
 
 function SideBox({ cardInfoData }: CardConfirmModalProps) {
   return (
     <S.SideContainer>
-      <div>담당자</div>
-      <S.Assignee>
-        <Image
-          src={cardInfoData.assignee.profileImageUrl}
-          alt={'담당자 이름'}
-          width={34}
-          height={34}
-        />
-        <div>{cardInfoData.assignee.nickname}</div>
-      </S.Assignee>
+      <S.AssigneeBox>
+        <S.Label>담당자</S.Label>
+        <S.Assignee>
+          <Image
+            src={cardInfoData.assignee.profileImageUrl}
+            alt={'담당자 이름'}
+            width={34}
+            height={34}
+          />
+          <S.NickName>{cardInfoData.assignee.nickname}</S.NickName>
+        </S.Assignee>
+      </S.AssigneeBox>
 
-      <div>마감일</div>
-      <div>{cardInfoData.dueDate}</div>
+      <S.Label>마감일</S.Label>
+      <S.DueDate>{cardInfoData.dueDate}</S.DueDate>
     </S.SideContainer>
   );
 }
