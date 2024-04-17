5c67f535cad5afb9810428c10fb96751974c7479
hyunjun9788 <namhyunjun9788@gmail.com>
2024-04-18 02:13:16 +0900

    feat: mock data 추가

---
+++ src/components/common/Modal/card-confirm/MockData.ts
@@ -0,0 +1,28 @@
+const MOCK_DATA = [
+  {
+    id: 1,
+    content: 'This is the first comment',
+    createdAt: '2024-04-17T10:30:00.423Z',
+    updatedAt: '2024-04-17T10:30:00.423Z',
+    cardId: 1,
+    author: {
+      profileImageUrl: 'https://i.ibb.co/ysRQMyj/me.jpg',
+      nickname: 'user1',
+      id: 1,
+    },
+  },
+  {
+    id: 2,
+    content: 'This is the second comment',
+    createdAt: '2024-04-17T11:15:00.423Z',
+    updatedAt: '2024-04-17T11:15:00.423Z',
+    cardId: 2,
+    author: {
+      profileImageUrl: 'https://i.ibb.co/VgZHtYL/Ellipse-39.png',
+      nickname: 'user2',
+      id: 2,
+    },
+  },
+];
+
+export default MOCK_DATA;
