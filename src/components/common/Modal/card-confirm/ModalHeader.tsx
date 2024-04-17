e44903bae50cd601036aded21c2b9253cf64a913
hyunjun9788 <namhyunjun9788@gmail.com>
2024-04-17 16:46:17 +0900

    refactor: 모달 헤더 타이틀 데이터 받아서 적용

--- src/components/common/Modal/card-confirm/ModalHeader.tsx
+++ src/components/common/Modal/card-confirm/ModalHeader.tsx
@@ -1,3 +1,4 @@
+import { CardConfirmModalProps } from './CardConfirmModal';
 import styled from 'styled-components';
 import CloseIcon from '@/public/icon/closeIcon.svg';
 import KebabIcon from '@/public/icon/kebabIcon.svg';
@@ -8,30 +9,34 @@ const S = {
     display: flex;
     justify-content: space-between;
   `,
+
   ModalTitle: styled.div`
     font-size: 2.4rem;
     font-weight: 700;
   `,
+
   HeaderButton: styled.div`
     display: flex;
     gap: 1rem;
   `,
+
   KebabIcon: styled(KebabIcon)`
     margin-top: 0.2rem;
     width: 3.2rem;
     height: 3.2rem;
     cursor: pointer;
   `,
+
   CloseIcon: styled(CloseIcon)`
     width: 3.2rem;
     height: 3.2rem;
     cursor: pointer;
   `,
 };
-function ModalHeader() {
+function ModalHeader({ cardInfoData }: CardConfirmModalProps) {
   return (
     <S.ModalHeader>
-      <S.ModalTitle>새로운 일정 관리 Taskify</S.ModalTitle>
+      <S.ModalTitle>{cardInfoData.title}</S.ModalTitle>
       <S.HeaderButton>
         <S.KebabIcon />
         <S.CloseIcon />
