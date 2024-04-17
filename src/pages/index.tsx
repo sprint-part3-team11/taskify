0949fb28f633ce59203d1c2485c8656d9e46deb8
박지윤 <tkrhkfld123@naver.com>
2024-04-17 13:20:04 +0900

    chore: 랜딩 페이지 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import React from 'react';
+import styled from 'styled-components';
+import Landing from '@/public/image/landing.png';
+
+const S = {};
+
 export default function Home() {
+  const router = useRouter();
+
+  const handleLoginClick = () => {
+    router.push('/login');
+  };
   return (
     <>
       <title>Taskify</title>
-      11팀 가보쟈궁~!~!
+      <Image src={Landing} width={400} height={100} alt="랜딩 메인 이미지" />
     </>
   );
 }
