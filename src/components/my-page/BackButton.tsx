dbbaa45e17271be922ade0aa1f69222b8c5bad77
박지윤 <tkrhkfld123@naver.com>
2024-04-19 00:58:52 +0900

    feat: 이전 버튼 구현

--- src/components/my-page/BackButton.tsx
+++ src/components/my-page/BackButton.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import styled from 'styled-components';
 import BackBtn from '@/public/icon/backButton.svg';
 
@@ -5,6 +6,8 @@ const S = {
   Container: styled.div`
     display: flex;
     align-items: center;
+    padding-bottom: 2.5rem;
+    cursor: pointer;
   `,
   BackText: styled.span`
     font-size: 1.6rem;
@@ -14,8 +17,14 @@ const S = {
 };
 
 function BackButton() {
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
   return (
-    <S.Container>
+    <S.Container onClick={handleGoBack}>
       <BackBtn />
       <S.BackText>돌아가기</S.BackText>
     </S.Container>
