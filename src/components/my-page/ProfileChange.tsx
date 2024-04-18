7ad5bff428fb89ec23a0783c675558feeefec06d
박지윤 <tkrhkfld123@naver.com>
2024-04-18 21:41:40 +0900

    chore: 프로필 이미지 변경 컴포넌트 생성

--- src/components/my-page/ProfileChange.tsx
+++ src/components/my-page/ProfileChange.tsx
@@ -1,5 +1,32 @@
+import { ImgFileUpload } from '../common/ImgFileUpload';
+import styled from 'styled-components';
+
 function ProfileChange() {
-  return <div>profilechange</div>;
+  const S = {
+    Container: styled.div``,
+    ImageBox: styled.div``,
+    ImageTitle: styled.span``,
+    ImageContent: styled.div``,
+    InputBox: styled.div``,
+    EmailLabel: styled.span``,
+    EmailInput: styled.div``,
+    NicknameLabel: styled.span``,
+    NicknameInput: styled.div``,
+  };
+  return (
+    <S.Container>
+      <S.ImageBox>
+        <S.ImageTitle>프로필</S.ImageTitle>
+        <S.ImageContent>
+          <ImgFileUpload edit small />
+        </S.ImageContent>
+      </S.ImageBox>
+
+      <S.InputBox>
+        <S.EmailLabel>이메일</S.EmailLabel>
+      </S.InputBox>
+    </S.Container>
+  );
 }
 
 export default ProfileChange;
