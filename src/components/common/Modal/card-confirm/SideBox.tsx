55d3e7e6addb937357ba6564fecb4aa8f9090d2f
hyunjun9788 <namhyunjun9788@gmail.com>
2024-04-19 01:26:55 +0900

    style: 모바일 일 때 Sidebox 스타일 적용

--- src/components/common/Modal/card-confirm/SideBox.tsx
+++ src/components/common/Modal/card-confirm/SideBox.tsx
@@ -7,36 +7,68 @@ const S = {
   SideContainer: styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: space-around;
     width: 20rem;
     height: 15rem;
     border: 1px solid ${({ theme }) => theme.color.grayLight};
     border-radius: 1rem;
     margin-left: 2.3rem;
     padding: 1.5rem 1.5rem 2.5rem 1.5rem;
+
+    ${MEDIA_QUERIES.onMobile} {
+      display: flex;
+      flex-direction: row;
+      width: 28rem;
+      height: 6.7rem;
+      margin-left: 0;
+      padding: 0;
+    }
   `,
 
   AssigneeBox: styled.div`
     display: flex;
     flex-direction: column;
+    ${MEDIA_QUERIES.onMobile} {
+      justify-content: center;
+      gap: 0.4rem;
+    }
   `,
 
   Label: styled.div`
     margin: 1rem 0;
     font-size: 1.2rem;
     font-weight: 600;
+
+    ${MEDIA_QUERIES.onMobile} {
+      margin: 0;
+      font-size: 1rem;
+    }
   `,
   Assignee: styled.div`
     display: flex;
     align-items: center;
+    ${MEDIA_QUERIES.onMobile} {
+      font-size: 1.2rem;
+    }
   `,
 
   NickName: styled.div`
     margin-left: 1rem;
   `,
-  DueDateBox: styled.div``,
+  DueDateBox: styled.div`
+    display: flex;
+    flex-direction: column;
+
+    ${MEDIA_QUERIES.onMobile} {
+      justify-content: center;
+      gap: 0.4rem;
+    }
+  `,
   DueDate: styled.div`
     font-size: 1.4rem;
+    ${MEDIA_QUERIES.onMobile} {
+      font-size: 1.2rem;
+    }
   `,
 };
 
