c78e25fb86c3f554e60ed25c50a0fce82eef41f5
hyunjun9788 <namhyunjun9788@gmail.com>
2024-04-18 22:23:14 +0900

    chore: 각 컴포넌트명 변경 Container->Layout

--- src/components/common/Modal/card-confirm/CardConfirmModal.tsx
+++ src/components/common/Modal/card-confirm/CardConfirmModal.tsx
@@ -3,30 +3,29 @@ import ModalHeader from './ModalHeader';
 import SideBox from './SideBox';
 import { CardConfirmModalProps } from './types';
 import styled from 'styled-components';
+import MEDIA_QUERIES from '@/constants/MEDIAQUERIES';
 
 const S = {
-  ModalContainer: styled.div`
+  ModalLayout: styled.div`
     padding: 3rem 2rem;
     width: 73rem;
     height: 76rem;
-
-    border: 1px solid gray;
   `,
 
-  Wrapper: styled.div`
+  ModalBody: styled.div`
     display: flex;
   `,
 };
 
 function CardConfirmModal({ cardInfoData }: CardConfirmModalProps) {
   return (
-    <S.ModalContainer>
+    <S.ModalLayout>
       <ModalHeader cardInfoData={cardInfoData} />
-      <S.Wrapper>
+      <S.ModalBody>
         <MainBox cardInfoData={cardInfoData} />
         <SideBox cardInfoData={cardInfoData} />
-      </S.Wrapper>
-    </S.ModalContainer>
+      </S.ModalBody>
+    </S.ModalLayout>
   );
 }
 
