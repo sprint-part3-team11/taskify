dc3cc2627f8f736d7be4ae83b1751a3e39855162
박지윤 <tkrhkfld123@naver.com>
2024-04-18 20:18:45 +0900

    style: 마이페이지 헤더 구현

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -1,16 +1,34 @@
+import styled from 'styled-components';
 import Sidebar from '@/components/common/SideBar';
+import MypageHeader from '@/components/my-page/MypageHeader';
 
+// 테스트용 배열
 const dashboards = [
   { id: '1', color: '#FFA500', name: '대시보드 1', createdByMe: true },
   { id: '2', color: '#FF2660', name: '대시보드 2', createdByMe: true },
   { id: '3', color: '#7AC555', name: '대시보드 3', createdByMe: false },
 ];
 
+const S = {
+  Container: styled.div`
+    display: flex;
+  `,
+  Side: styled(Sidebar)`
+    flex: 0 0 auto;
+  `,
+  ContentContainer: styled.div`
+    flex: 1;
+  `,
+};
+
 function mypage() {
   return (
-    <>
-      <Sidebar dashboards={dashboards} />
-    </>
+    <S.Container>
+      <S.Side dashboards={dashboards} />
+      <S.ContentContainer>
+        <MypageHeader name="공주들" />
+      </S.ContentContainer>
+    </S.Container>
   );
 }
 
