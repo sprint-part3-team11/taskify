50bddf8590c140ac2eb9d949537cef9365b24c02
hyunjun9788 <namhyunjun9788@gmail.com>
2024-04-18 23:18:33 +0900

    style: 모바일 일 때 이미지 영역 너비 조정

--- src/components/common/Modal/card-confirm/ContentAndImageBox.tsx
+++ src/components/common/Modal/card-confirm/ContentAndImageBox.tsx
@@ -1,23 +1,35 @@
 import Image from 'next/image';
 import { CardConfirmModalProps } from './types';
 import styled from 'styled-components';
+import MEDIA_QUERIES from '@/constants/MEDIAQUERIES';
 
 const S = {
   Container: styled.div`
     display: flex;
     flex-direction: column;
     width: 100%;
+
+    ${MEDIA_QUERIES.onMobile} {
+      width: 100%;
+      word-break: break-all;
+    }
   `,
   ContentBox: styled.div`
     width: 100%;
     margin-bottom: 2.5rem;
+  `,
+  Image: styled(Image)`
+    ${MEDIA_QUERIES.onMobile} {
+      width: 100%;
+      height: 16rem;
+    }
   `,
 };
 function ContentAndImageBox({ cardInfoData }: CardConfirmModalProps) {
   return (
     <S.Container>
       <S.ContentBox>{cardInfoData.description}</S.ContentBox>
-      <Image
+      <S.Image
         src={cardInfoData.imageUrl}
         width={450}
         height={260}
