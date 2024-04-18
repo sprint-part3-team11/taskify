dd8ce6e01eeb3b5668f05299af095455c1dee60b
sihyonn <sihyeon982@naver.com>
2024-04-18 22:14:04 +0900

    fix: SelectBox 선택지 밀려올라가는 이슈 해결

--- src/components/common/SelectBox.tsx
+++ src/components/common/SelectBox.tsx
@@ -57,7 +57,7 @@ const S = {
     border: 1px solid ${({ theme }) => theme.color.grayLight};
     background: ${({ theme }) => theme.color.white};
     box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.08);
-    margin-top: 0.2rem;
+    margin-top: 8rem;
 
     ${MEDIA_QUERIES.onTablet} {
       width: 21.7rem;
