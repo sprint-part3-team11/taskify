cc245132efa36794c27ffcda0915eac4b4b32e23
박지윤 <tkrhkfld123@naver.com>
2024-04-19 18:28:02 +0900

    chore: 대쉬보드 사이드바 복구

---
+++ src/components/common/DashBoardHeader/SideBar.tsx
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import MEDIA_QUERIES from '@/constants/MEDIAQUERIES';
+
+//테스트용
+const S = {
+  SideBarBox: styled.div`
+    position: fixed;
+    top: 0;
+    border: 1px solid black;
+    width: 30rem;
+    min-height: 100vh;
+
+    ${MEDIA_QUERIES.onTablet} {
+      width: 16rem;
+      min-height: 100vh;
+    }
+    ${MEDIA_QUERIES.onMobile} {
+      width: 6.7rem;
+      min-height: 100vh;
+    }
+  `,
+};
+
+function SideBar() {
+  return (
+    <div>
+      <S.SideBarBox />
+    </div>
+  );
+}
+
+export default SideBar;
