65ff2d38b0629e02b66e4fff9b7c4ae0cf7eb636
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-04-22 17:51:27 +0900

    style: FormLayout 스타일 구현

--- src/components/form/FormLayout.tsx
+++ src/components/form/FormLayout.tsx
@@ -2,6 +2,55 @@ import Link from 'next/link';
 import styled from 'styled-components';
 import LogoText from '@/public/icon/Taskify.svg';
 import LogoImg from '@/public/icon/logoImg.svg';
+
+const S = {
+  Layout: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    background-color: ${({ theme }) => theme.color.background};
+  `,
+  Container: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 2.4rem;
+    width: 52rem;
+  `,
+  LogoBox: styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1.5rem;
+    margin-bottom: 1.4rem;
+  `,
+  LogoImg: styled(LogoImg)`
+    margin: 0 0 1.7rem 3.9rem;
+  `,
+  WelcomeText: styled.h2`
+    color: ${({ theme }) => theme.color.body};
+    text-align: center;
+    font-size: 2rem;
+    font-weight: 500;
+  `,
+  LinkBox: styled.div`
+    display: flex;
+    gap: 1rem;
+
+    color: ${({ theme }) => theme.color.body};
+    text-align: center;
+  `,
+  LinkText: styled(Link)`
+    color: ${({ theme }) => theme.color.main};
+    text-decoration-line: underline;
+  `,
+};
+
 interface FormLayoutProps {
   welcomeText?: string;
   children: React.ReactNode;
