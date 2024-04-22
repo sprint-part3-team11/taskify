e2c98777c43ccc65dceafef9ab858a8a299b56b9
sihyonn <sihyeon982@naver.com>
2024-04-22 19:14:41 +0900

    comment: 테스트용 주석 제거

--- src/api/cards.api.ts
+++ src/api/cards.api.ts
@@ -9,12 +9,7 @@ const getCardList = (accessToken: string, columnId: string) => {
   return instance({
     url: API.CARDS,
     method: 'GET',
-    headers: {
-      // 요청 헤더에 액세스 토큰 추가
-      Authorization: accessToken ? `Bearer ${accessToken}` : '', // 이미 interceptor에 설정해서 삭제 예정
-    },
     params: {
-      size: 5,
       columnId: columnId,
     },
   });
