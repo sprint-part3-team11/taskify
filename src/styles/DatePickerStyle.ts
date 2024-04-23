32cc3c6187b5513d9201ce47be3a163401641d02
박지윤 <tkrhkfld123@naver.com>
2024-04-23 21:28:52 +0900

    chore: 삭제된 파일 복구

---
+++ src/styles/DatePickerStyle.ts
@@ -0,0 +1,26 @@
+import { css } from 'styled-components';
+
+const datePickerStyle = css`
+  .react-datepicker-wrapper {
+    width: 100%;
+  }
+  .react-datepicker__input-container {
+    display: flex;
+    align-items: center;
+    width: 100%;
+  }
+  .react-datepicker__input-container {
+    svg {
+      padding: 1.6rem;
+    }
+    input {
+      padding: 1.6rem 3.5rem;
+
+      &::placeholder {
+        color: ${({ theme }) => theme.color.gray};
+      }
+    }
+  }
+`;
+
+export default datePickerStyle;
