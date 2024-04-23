d723ef93ab9861306c805d167652f5cb4e97ad9d
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-04-23 16:04:13 +0900

    feat: 로그인/회원가입 페이지 UI 구현

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,5 +1,14 @@
-function SignIn() {
-  return <div />;
+import Form from '@/components/common/form/Form';
+import SignLayout from '@/components/template/SignLayout';
+
+function signin() {
+  return (
+    <div>
+      <SignLayout pageType="signIn">
+        <Form formType="signIn" />
+      </SignLayout>
+    </div>
+  );
 }
 
-export default SignIn;
+export default signin;
