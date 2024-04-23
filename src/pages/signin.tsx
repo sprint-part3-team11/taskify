7c00f25ede731c0b781e3610ad95e7924de48ff6
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-04-24 00:17:10 +0900

    feat: 로그인 post API 요청 & my-dashboard로 리다이렉트

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,14 +1,31 @@
+import { useRouter } from 'next/router';
 import Form from '@/components/common/form/Form';
 import SignLayout from '@/components/template/SignLayout';
+import authApi from '@/api/auth.api';
 
-function signin() {
+function SignIn() {
+  const router = useRouter();
+
+  const loginUser = async (data) => {
+    try {
+      const response = await authApi.getLogin({
+        email: data.email,
+        password: data.password,
+      });
+      localStorage.setItem('accessToken', response.data.accessToken);
+      router.push('/my-dashboard');
+    } catch (error) {
+      console.error('에러', error.response.data.message);
+    }
+  };
+
   return (
     <div>
       <SignLayout pageType="signIn">
-        <Form formType="signIn" />
+        <Form formType="signIn" onSubmit={loginUser} />
       </SignLayout>
     </div>
   );
 }
 
-export default signin;
+export default SignIn;
