4ea9ab2e87ba7b08c46f8920c0de57a095899652
박지윤 <tkrhkfld123@naver.com>
2024-04-23 15:44:19 +0900

    chore: 대쉬보드 api 생성

--- src/api/dashboards.api.ts
+++ src/api/dashboards.api.ts
@@ -4,11 +4,30 @@ import instance from '@/api/instance';
 /**
  * 대시보드 목록 조회
  */
-const getDashboardList = () => {
+const getDashboardList = ({ navigationMethod, page, size }) => {
   return instance({
     url: API.DASHBOARDS,
     method: 'GET',
+    params: {
+      navigationMethod,
+      page,
+      size,
+    },
   });
 };
 
-export default { getDashboardList };
+/**
+ * 대시보드 생성
+ */
+const getCreatedDashboard = ({ title, color }) => {
+  return instance({
+    url: API.DASHBOARDS,
+    method: 'POST',
+    data: {
+      title,
+      color,
+    },
+  });
+};
+
+export default { getDashboardList, getCreatedDashboard };
