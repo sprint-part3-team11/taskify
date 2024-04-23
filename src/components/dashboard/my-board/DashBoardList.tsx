62c61a02fbb8a0b808276ed6f638af3f9609994a
sihyonn <sihyeon982@naver.com>
2024-04-23 20:44:29 +0900

    feat: 나의 대시보드 대시보드 목록 api 연동

--- src/components/dashboard/my-board/DashBoardList.tsx
+++ src/components/dashboard/my-board/DashBoardList.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 import { styled } from 'styled-components';
 import CircleColor from '@/components/common/CircleColor';
 import AddIconButton from '@/components/common/button/AddIconButton';
 import MEDIA_QUERIES from '@/constants/MEDIAQUERIES';
+import dashboardsApi from '@/api/dashboards.api';
 import CrownIcon from '@/public/icon/crownIcon.svg';
 import RightArrow from '@/public/icon/rightArraowIcon.svg';
 import { GridTemplate } from '@/styles/CommonStyle';
@@ -39,60 +41,30 @@ const S = {
   `,
 };
 
-const dashboards = [
-  {
-    id: 1,
-    title: '이게맞냐구',
-    color: '#76A5EA',
-    createdAt: '2024-04-21T14:07:17.165Z',
-    updatedAt: '2024-04-21T14:07:17.165Z',
-    createdByMe: true,
-    userId: 1,
-  },
-  {
-    id: 2,
-    title: '나는 괜찮아',
-    color: '#E876EA',
-    createdAt: '2024-04-21T14:07:17.165Z',
-    updatedAt: '2024-04-21T14:07:17.165Z',
-    createdByMe: false,
-    userId: 1,
-  },
-  {
-    id: 3,
-    title: '이게맞냐구',
-    color: '#76A5EA',
-    createdAt: '2024-04-21T14:07:17.165Z',
-    updatedAt: '2024-04-21T14:07:17.165Z',
-    createdByMe: true,
-    userId: 1,
-  },
-  {
-    id: 4,
-    title: '나는 괜찮아',
-    color: '#E876EA',
-    createdAt: '2024-04-21T14:07:17.165Z',
-    updatedAt: '2024-04-21T14:07:17.165Z',
-    createdByMe: false,
-    userId: 1,
-  },
-  {
-    id: 5,
-    title: '이게맞냐구',
-    color: '#76A5EA',
-    createdAt: '2024-04-21T14:07:17.165Z',
-    updatedAt: '2024-04-21T14:07:17.165Z',
-    createdByMe: true,
-    userId: 1,
-  },
-];
-
 function DashBoardList() {
+  const [dashboards, setDashboards] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await dashboardsApi.getDashboardList('infiniteScroll');
+        setDashboards(response.data); // 받은 데이터를 dashboards 상태로 설정
+      } catch (error) {
+        console.error('목록조회 오류:', error.response.data.message);
+      }
+    };
+
+    fetchData(); // 컴포넌트가 마운트되었을 때 데이터를 가져오도록 호출
+  }, []);
+
+  const boards = dashboards.dashboards;
+  console.log('대시보드 데이터: ', boards);
+
   return (
     <GridTemplate>
       <AddIconButton style={{ height: '7rem' }}>새로운 대시보드</AddIconButton>
-      {dashboards.map((board) => (
-        <S.Container>
+      {boards?.map((board) => (
+        <S.Container key={board.id}>
           <S.BoardTitle>
             <CircleColor color={board.color} />
             <div>
@@ -100,7 +72,7 @@ function DashBoardList() {
               {board.createdByMe && <CrownIcon className="crown" />}
             </div>
           </S.BoardTitle>
-          <Link href={`/my-dashboards/${board.id}`}>
+          <Link href={`/dashboard/${board.id}`}>
             <RightArrow />
           </Link>
         </S.Container>
