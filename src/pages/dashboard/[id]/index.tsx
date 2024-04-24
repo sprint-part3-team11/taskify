4c6af68b927c2cc2dda354c5b21a2f044e2518ba
MinJoon_Choi <minjoon1998@gmail.com>
2024-04-25 02:36:07 +0900

    feat: 컬럼 데이터 로직 추가(GET, POST)

--- src/pages/dashboard/[id]/index.tsx
+++ src/pages/dashboard/[id]/index.tsx
@@ -1,5 +1,6 @@
+import { useRouter } from 'next/router';
 import { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import AddIconButton from '@/components/common/button/AddIconButton';
 import NewColumnsModal from '@/components/common/modal/NewColumnsModal';
 import Column from '@/components/dashboard/column/Column';
@@ -7,22 +8,47 @@ import PageLayout from '@/components/template/PageLayout';
 import MEDIA_QUERIES from '@/constants/MEDIAQUERIES';
 import columnsApi from '@/api/columns.api';
 
-// 테스트용 배열
-const dashboards = [
-  { id: '1', color: '#FFA500', name: '대시보드 1', createdByMe: true },
-  { id: '2', color: '#FF2660', name: '대시보드 2', createdByMe: true },
-  { id: '3', color: '#7AC555', name: '대시보드 3', createdByMe: false },
-];
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-100%);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+    transform: translateX(0);
+  }
+  to {
+    opacity: 0;
+    transform: translateX(100%);
+  }
+`;
 
 const S = {
   DashBoardWrapper: styled.div`
     display: flex;
+    /* margin-top: 7rem; */
+    /* margin-left: 30rem; */
+
     ${MEDIA_QUERIES.onTablet} {
       display: block;
     }
     ${MEDIA_QUERIES.onMobile} {
       display: block;
     }
+
+    /* &::-webkit-scrollbar {
+      position: fixed;
+      top: 10px;
+      right: 0;
+      width: 10px;
+    } */
   `,
   IconWrapper: styled.div`
     margin: 6.8rem 0rem 0rem 2rem;
@@ -51,46 +77,31 @@ const S = {
 };
 
 export default function DashBoard() {
-  const [columnList, setColumnList] = useState([
-    {
-      id: 19980,
-      title: 'To do',
-      teamId: '4-11',
-      dashboardId: 5941,
-      createdAt: '2024-04-16T16:26:10.172Z',
-      updatedAt: '2024-04-16T16:26:10.172Z',
-    },
-    {
-      id: 19981,
-      title: 'On progress',
-      teamId: '4-11',
-      dashboardId: 5941,
-      createdAt: '2024-04-16T16:26:10.172Z',
-      updatedAt: '2024-04-16T16:26:10.172Z',
-    },
-    {
-      id: 19982,
-      title: 'Done',
-      teamId: '4-11',
-      dashboardId: 5941,
-      createdAt: '2024-04-16T16:26:10.172Z',
-      updatedAt: '2024-04-16T16:26:10.172Z',
-    },
-  ]);
+  const [columnList, setColumnList] = useState([]);
   const initialColumnsCount = useRef(columnList.length);
   const [isModalOpen, setModalOpen] = useState(false);
   const scrollRef = useRef(null);
 
+  const router = useRouter();
+  const dashboardId = +router.query.id;
+
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
 
-  const handleCreate = (columnName: string) => {
-    const newColumn = {
-      id: columnList.length + 1,
-      title: columnName,
-    };
-    setColumnList([...columnList, newColumn]);
-    setModalOpen(false);
+  const handleCreate = async (columnName: string) => {
+    try {
+      const response = await columnsApi.postCreateColumn({
+        title: columnName,
+        dashboardId,
+      });
+
+      const newColumn = response.data;
+      setColumnList([...columnList, newColumn]);
+      console.log(columnList);
+      setModalOpen(false);
+    } catch (error) {
+      console.error('컬럼 생성 에러:', error);
+    }
   };
 
   useEffect(() => {
@@ -186,16 +197,20 @@ export default function DashBoard() {
   // }, []);
 
   useEffect(() => {
+    if (!router.isReady || !dashboardId) return;
+
     const fetchData = async () => {
       try {
-        const response = await columnsApi.getColumnList('5941');
-        console.log(response.data.data);
+        const response = await columnsApi.getColumnList(dashboardId);
+        setColumnList(response.data.data);
+        // console.log(response.data.data);
       } catch (error) {
         console.error('컬럼에러:', error.message);
+        setColumnList([]);
       }
     };
     fetchData();
-  }, []);
+  }, [router.isReady, router.query.id]);
 
   // {id: 19980, title: 'To do', teamId: '4-11', dashboardId: 5941, createdAt: '2024-04-16T16:26:10.172Z', …}
   // {id: 19981, title: 'On progress', teamId: '4-11', dashboardId: 5941, createdAt: '2024-04-16T16:26:10.172Z', …}
@@ -215,8 +230,14 @@ export default function DashBoard() {
             ref={index === columnList.length - 1 ? scrollRef : null}
           />
         ))}
+        {/* <Column />
+        <Column />
+        <Column />
+        <Column /> */}
         <S.IconWrapper>
-          <S.AddIconButton onClick={openModal}>새로운 대시보드</S.AddIconButton>
+          <S.AddIconButton onClick={openModal}>
+            새로운 컬럼 추가하기
+          </S.AddIconButton>
         </S.IconWrapper>
         <NewColumnsModal
           isOpen={isModalOpen}
