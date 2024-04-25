d982a0346c87b66ec301dfd3f4dccdd010489f0b
박지윤 <tkrhkfld123@naver.com>
2024-04-26 00:01:06 +0900

    chore: 셀렉트박스 연습해보기

--- src/pages/jy.tsx
+++ src/pages/jy.tsx
@@ -30,11 +30,98 @@ const selectBoxOptions = [
   { id: 34, nickname: '박지윤' },
   { id: 35, nickname: 'alallalalalaalalallalalalalaaalalalaalal' },
 ];
+
+const data = [
+  {
+    id: 19980,
+    title: 'To do',
+    teamId: '4-11',
+    dashboardId: 5941,
+    createdAt: '2024-04-16T16:26:10.172Z',
+    updatedAt: '2024-04-16T16:26:10.172Z',
+  },
+  {
+    id: 19981,
+    title: 'On progress',
+    teamId: '4-11',
+    dashboardId: 5941,
+    createdAt: '2024-04-16T16:26:10.172Z',
+    updatedAt: '2024-04-16T16:26:10.172Z',
+  },
+  {
+    id: 19982,
+    title: 'Done',
+    teamId: '4-11',
+    dashboardId: 5941,
+    createdAt: '2024-04-16T16:26:10.172Z',
+    updatedAt: '2024-04-16T16:26:10.172Z',
+  },
+  {
+    id: 19985,
+    title: '아침',
+    teamId: '4-11',
+    dashboardId: 5941,
+    createdAt: '2024-04-17T02:04:51.592Z',
+    updatedAt: '2024-04-17T02:04:51.592Z',
+  },
+  {
+    id: 19986,
+    title: '점심',
+    teamId: '4-11',
+    dashboardId: 5941,
+    createdAt: '2024-04-17T02:05:02.991Z',
+    updatedAt: '2024-04-17T02:05:02.991Z',
+  },
+  {
+    id: 22152,
+    title: '민준',
+    teamId: '4-11',
+    dashboardId: 5941,
+    createdAt: '2024-04-24T12:52:57.690Z',
+    updatedAt: '2024-04-24T12:52:57.690Z',
+  },
+  {
+    id: 22406,
+    title: '민준',
+    teamId: '4-11',
+    dashboardId: 5941,
+    createdAt: '2024-04-24T15:47:52.041Z',
+    updatedAt: '2024-04-24T15:47:52.041Z',
+  },
+  {
+    id: 23232,
+    title: 'string',
+    teamId: '4-11',
+    dashboardId: 5941,
+    createdAt: '2024-04-25T05:07:17.931Z',
+    updatedAt: '2024-04-25T05:07:17.931Z',
+  },
+  {
+    id: 23696,
+    title: '되야지',
+    teamId: '4-11',
+    dashboardId: 5941,
+    createdAt: '2024-04-25T17:15:32.495Z',
+    updatedAt: '2024-04-25T17:15:32.495Z',
+  },
+  {
+    id: 23697,
+    title: '되야지',
+    teamId: '4-11',
+    dashboardId: 5941,
+    createdAt: '2024-04-25T17:15:40.033Z',
+    updatedAt: '2024-04-25T17:15:40.033Z',
+  },
+];
 function jy() {
   return (
     <>
-      {/* true면 ={true} 를 린트가 쓰지 말라고 빨간줄 나오네요! false일때만 false를 명시해주세요 */}
-      <SelectBox options={selectBoxOptions} placeholder />
+      <SelectBox
+        options={selectBoxOptions}
+        placeholder
+        displayFieldName="nickname"
+      />
+      <SelectBox options={data} placeholder displayFieldName="title" />
     </>
   );
 }
