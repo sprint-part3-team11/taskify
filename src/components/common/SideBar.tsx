1b14d2f49e3bdbec539a663f2a9881feb78e931d
박지윤 <tkrhkfld123@naver.com>
2024-04-27 23:59:50 +0900

    chore: 사이드바 페이지 네이션 위치 조정

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -122,6 +122,11 @@ const S = {
   `,
   PageNavigationBox: styled.div`
     display: flex;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    transform: translateY(-1rem);
     flex-direction: column;
     justify-content: center;
     align-items: center;
