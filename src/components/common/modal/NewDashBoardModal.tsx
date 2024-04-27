6131faacece48856069e41ac91122c739b20de92
MinJoon_Choi <minjoon1998@gmail.com>
2024-04-27 22:20:35 +0900

    chore: 오타 수정

--- src/components/common/modal/NewDashBoardModal.tsx
+++ src/components/common/modal/NewDashBoardModal.tsx
@@ -75,7 +75,7 @@ function NewDashBoardModal({
   const [dashBoardName, setColumnName] = useState('');
   const selectedColor = useRecoilValue(resultColorState);
   const setColor = useSetRecoilState(resultColorState);
-  const { mutate: creatDashboard } = useCreateDashboardMutation();
+  const { mutate: createDashboard } = useCreateDashboardMutation();
   console.log(dashBoardName);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -90,7 +90,7 @@ function NewDashBoardModal({
 
   const handleDashBoardName = async () => {
     onCreate(dashBoardName, selectedColor);
-    creatDashboard({ title: dashBoardName, color: selectedColor });
+    createDashboard({ title: dashBoardName, color: selectedColor });
     handleClose();
   };
 
