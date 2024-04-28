d8e3d802aaf1b9c9a7959b8b77d844ce46f84106
MinJoon_Choi <minjoon1998@gmail.com>
2024-04-29 04:06:23 +0900

    refactor: useLoadInvitationQuery 타입 지정

--- src/hooks/query/dashboards/useLoadInvitationQuery.ts
+++ src/hooks/query/dashboards/useLoadInvitationQuery.ts
@@ -2,7 +2,17 @@ import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { API } from '@/constants/API';
 import dashboardsApi from '@/api/dashboards.api';
 
-function useLoadInvitationQuery({ dashboardId, page, size = 4 }) {
+interface LoadInvitationQueryProp {
+  dashboardId: number;
+  page: number;
+  size: number;
+}
+
+function useLoadInvitationQuery({
+  dashboardId,
+  page,
+  size = 4,
+}: LoadInvitationQueryProp) {
   const queryClient = useQueryClient();
 
   return useQuery({
