291adc43b8a105756cd22f384bd8437b7952ecb8
MinJoon_Choi <minjoon1998@gmail.com>
2024-04-29 03:50:59 +0900

    refactor: useColumnListQuery 타입 지정

--- src/hooks/query/columns/useColumnListQuery.ts
+++ src/hooks/query/columns/useColumnListQuery.ts
@@ -2,8 +2,12 @@ import { useQuery } from '@tanstack/react-query';
 import { API } from '@/constants/API';
 import columnsApi from '@/api/columns.api';
 
+interface ColumnListQueryProp {
+  dashboardId: number;
+}
+
 // 컬럼 목록조회
-function useColumnListQuery({ dashboardId }) {
+function useColumnListQuery({ dashboardId }: ColumnListQueryProp) {
   return useQuery({
     queryKey: [API.COLUMNS, dashboardId],
     queryFn: async () => {
