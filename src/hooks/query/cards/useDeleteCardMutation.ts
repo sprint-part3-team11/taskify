bd58067dd8189545c5277b2c58e18d75b3985955
MinJoon_Choi <minjoon1998@gmail.com>
2024-04-29 03:49:03 +0900

    refactor: useDeleteCardMutation 타입 지정

--- src/hooks/query/cards/useDeleteCardMutation.ts
+++ src/hooks/query/cards/useDeleteCardMutation.ts
@@ -2,17 +2,21 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { API } from '@/constants/API';
 import cardsApi from '@/api/cards.api';
 
+interface CardIdProp {
+  cardId: number;
+}
+
 function useDeleteCardMutation() {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: async ({ cardId }) => {
+    mutationFn: async ({ cardId }: CardIdProp) => {
       return cardsApi.deleteCard({
         cardId,
       });
     },
     onSuccess: () => {
-      queryClient.invalidateQueries([API.CARDS]);
+      queryClient.invalidateQueries({ queryKey: [API.CARDS] });
     },
   });
 }
