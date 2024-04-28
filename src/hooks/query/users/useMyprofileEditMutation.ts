ac9e8a98d56e06face55bdbee42eac6e017140b6
박지윤 <tkrhkfld123@naver.com>
2024-04-29 02:04:36 +0900

    fix: 프로필 타입 에러 해결

--- src/hooks/query/users/useMyprofileEditMutation.ts
+++ src/hooks/query/users/useMyprofileEditMutation.ts
@@ -1,7 +1,12 @@
+import toast from 'react-hot-toast';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useRecoilValue } from 'recoil';
 import { profileImageUrlState } from '@/hooks/query/users/useMyPropfileQuery';
 import usersApi from '@/api/users.api';
+
+interface ProfileEditData {
+  name: string;
+}
 
 // 프로필 수정 => 이미지, 닉네임
 function useProfileEditMutation(imgServerUrl: string) {
@@ -9,14 +14,14 @@ function useProfileEditMutation(imgServerUrl: string) {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: async (data) => {
+    mutationFn: async (data: ProfileEditData) => {
       return usersApi.putMyProfileEdit({
         nickname: data.name,
         profileImageUrl: imgServerUrl || profileImageUrl,
       });
     },
     onSuccess: () => {
-      alert('성공');
+      toast.success('성공');
       queryClient.invalidateQueries();
     },
   });
