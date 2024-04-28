c7288592307690dbe036e455289341c0e3a846be
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-04-28 21:20:53 +0900

    refactor: 비밀번호 변경 에러 수정

--- src/hooks/query/auth/usePasswordChangeMutation.ts
+++ src/hooks/query/auth/usePasswordChangeMutation.ts
@@ -8,17 +8,17 @@ function usePasswordChangeMutation() {
   const [modalMessage, setModalMessage] = useState('');
   const mutation = useMutation({
     mutationFn: async (data) => {
-      return authApi.getPasswordChange({
+      return authApi.putPasswordChange({
         password: data.nowPassword,
         newPassword: data.newPassword,
       });
     },
     onSuccess: () => {
-      alert('비밀번호 변경 성공');
+      alert('비밀번호 변경 성공✨');
     },
     onError: (error) => {
       setOpen(true);
-      const message = error.response?.data?.message || '오류가 발생했습니다.';
+      const message = error.response?.data?.message;
       setModalMessage(message);
     },
   });
