6e2497928d40358e2303cbb093e13dce1952c363
박지윤 <tkrhkfld123@naver.com>
2024-04-28 11:48:42 +0900

    fix: 사이드바 페이지 초기화 및 active 효과 주기

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import CircleColor from '@/components/common/CircleColor';
 import NewDashBoardModal from '@/components/common/modal/NewDashBoardModal';
-import useCreateDashboardMutation from '@/hooks/query/dashboards/useCreateDashboardMutation';
 import useDashboardListQuery from '@/hooks/query/dashboards/useDashboardListQuery';
 import MEDIA_QUERIES from '@/constants/MEDIAQUERIES';
 import AddDashBoardIcon from '@/public/icon/addDashboardBox.svg';
@@ -95,6 +94,10 @@ const S = {
     margin-top: 0.85rem;
     padding: 10px;
     cursor: pointer;
+    border-radius: 0.55rem;
+    background: ${({ isActive }) =>
+      isActive ? 'var(--violet-violet-8, #f1effd)' : 'transparent'};
+    transition: all 0.1s ease-in-out;
 
     &:hover {
       border-radius: 0.55rem;
@@ -174,8 +177,8 @@ function Sidebar() {
   const openModal6 = () => setModalOpen6(true);
   const closeModal6 = () => setModalOpen6(false);
   const router = useRouter();
-
   const [page, setPage] = useState(1);
+  const [selectedDashboardId, setSelectedDashboardId] = useState('');
 
   const { data } = useDashboardListQuery({
     navigationMethod: 'pagination',
@@ -205,6 +208,8 @@ function Sidebar() {
   };
 
   const handleDashboardClick = (dashboardId: string) => {
+    setPage(page);
+    setSelectedDashboardId(dashboardId);
     router.push(`/dashboard/${dashboardId}`);
   };
 
@@ -223,6 +228,7 @@ function Sidebar() {
           <S.DashboardItemWrapper
             key={dashboard.id}
             onClick={() => handleDashboardClick(dashboard.id)}
+            isActive={dashboard.id === selectedDashboardId}
           >
             <S.CircleColor>
               <CircleColor color={dashboard.color} />
