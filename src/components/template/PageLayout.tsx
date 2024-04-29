83132202d0cfecfcd7cd8f5603e8e81e36011036
MinJoon_Choi <minjoon1998@gmail.com>
2024-04-29 21:02:43 +0900

    refactor: PageLayout mock data 삭제

--- src/components/template/PageLayout.tsx
+++ src/components/template/PageLayout.tsx
@@ -3,12 +3,6 @@ import Sidebar from '@/components/common/SideBar';
 import DashBoardHeader from '@/components/dashboard/DashBoardHeader';
 import MEDIA_QUERIES from '@/constants/MEDIAQUERIES';
 
-const dashboards = [
-  { id: '1', color: '#FFA500', name: '대시보드 1', createdByMe: true },
-  { id: '2', color: '#FF2660', name: '대시보드 2', createdByMe: true },
-  { id: '3', color: '#7AC555', name: '대시보드 3', createdByMe: false },
-];
-
 const S = {
   LayoutContainer: styled.div`
     overflow: auto;
@@ -43,46 +37,10 @@ function PageLayout({
   openInviteModal?: () => void;
   myPage: boolean;
 }) {
-  const MY_IMAGE_URL = 'https://i.ibb.co/ysRQMyj/me.jpg';
-  const invitedUsers = [
-    {
-      id: 1,
-      profileImageUrl: 'https://i.ibb.co/kgykYbx/Ellipse-40.png',
-    },
-    {
-      id: 2,
-      profileImageUrl: 'https://i.ibb.co/tPyNYb1/Ellipse-38.png',
-    },
-    {
-      id: 3,
-      profileImageUrl: 'https://i.ibb.co/VgZHtYL/Ellipse-39.png',
-    },
-    {
-      id: 4,
-      profileImageUrl: 'https://i.ibb.co/VgZHtYL/Ellipse-39.png',
-    },
-    {
-      id: 5,
-      profileImageUrl: 'https://i.ibb.co/VgZHtYL/Ellipse-39.png',
-    },
-    {
-      id: 6,
-      profileImageUrl: 'https://i.ibb.co/VgZHtYL/Ellipse-39.png',
-    },
-  ];
-
   return (
     <S.LayoutContainer>
-      <Sidebar dashboards={dashboards} />
-      <DashBoardHeader
-        dashboardName={dashboards[0].name}
-        createdByMe={dashboards[0].createdByMe}
-        profileName="남현준"
-        profileImgURL={MY_IMAGE_URL}
-        invitedUsers={invitedUsers}
-        openInviteModal={openInviteModal}
-        myPage={myPage}
-      />
+      <Sidebar />
+      <DashBoardHeader myPage={myPage} />
       <S.BodyContainer>{children}</S.BodyContainer>
     </S.LayoutContainer>
   );
