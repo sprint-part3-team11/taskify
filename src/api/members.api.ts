fa6c4474a4d68a32a6345ba4456211172895a673
이지수 <144462290+elenfl0122@users.noreply.github.com>
2024-04-29 23:21:35 +0900

    fix: members.api.ts 타입 지정

--- src/api/members.api.ts
+++ src/api/members.api.ts
@@ -1,11 +1,20 @@
 import { API, API_MEMBERS } from '@/constants/API';
 import instance from '@/api/instance';
 
+interface GetDashboardMembersProps {
+  dashboardId: number;
+  page: number;
+  size: number;
+}
+
 /**
  * 대시보드 멤버 목록 조회
  */
-
-const getDashboardMembers = (dashboardId, page = 1, size = 30) => {
+const getDashboardMembers = ({
+  dashboardId,
+  page = 1,
+  size = 30,
+}: GetDashboardMembersProps) => {
   return instance({
     url: API.MEMBERS,
     method: 'GET',
@@ -17,7 +26,7 @@ const getDashboardMembers = (dashboardId, page = 1, size = 30) => {
   });
 };
 
-const deleteDashboardMembers = (memberId) => {
+const deleteDashboardMembers = (memberId: number) => {
   return instance({
     url: API_MEMBERS.BY_ID(memberId),
     method: 'DELETE',
