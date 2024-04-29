ed50fa36a6ccd894d8e56a46ff48ed6d7b6e9d68
박지윤 <tkrhkfld123@naver.com>
2024-04-29 20:19:06 +0900

    fix: 셀렉트 박스 타입 해결

--- src/components/common/SelectBox.tsx
+++ src/components/common/SelectBox.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import useOutSideClick from '@/hooks/useClickOutside';
@@ -98,7 +99,7 @@ const S = {
 
 interface Option {
   id: number;
-  [key: string]: unknown;
+  [key: string]: any;
 }
 interface SelectBoxProps {
   options: Option[];
@@ -115,13 +116,15 @@ function SelectBox({
   displayFieldName,
   initialValue,
 }: SelectBoxProps): JSX.Element {
-  const [selectedOption, setSelectedOption] = useState(null);
+  const [selectedOption, setSelectedOption] = useState<any>(null);
   const [isFocused, setIsFocused] = useState(false);
   const [filterText, setFilterText] = useState('');
   const optionAreaRef = useRef<HTMLUListElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
-  function handleSelectOption(option: never): void {
+  console.log(options);
+
+  function handleSelectOption(option: any): void {
     setSelectedOption(option);
     setFilterText('');
     setIsFocused(false);
@@ -153,6 +156,7 @@ function SelectBox({
         ref={inputRef}
         type="text"
         value={
+          // eslint-disable-next-line no-nested-ternary
           selectedOption
             ? selectedOption[displayFieldName]
             : !isFocused && initialValue
