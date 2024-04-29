b13ffe02fa2288f928df6a6fb9f46519ed36d77a
박지윤 <tkrhkfld123@naver.com>
2024-04-29 20:31:25 +0900

    fix 아바타 리스트 타입에러 해결

--- src/components/dashboard/AvatarList.tsx
+++ src/components/dashboard/AvatarList.tsx
@@ -1,9 +1,19 @@
-import { useEffect, useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import AvatarGroup from '@mui/material/AvatarGroup';
 import theme from '@/styles/theme';
 
-function AvatarList({ max, dataArr }) {
+interface AvatarProps {
+  max: number;
+  dataArr: AvatarData[];
+}
+
+interface AvatarData {
+  nickname: string;
+  profileImageUrl: string;
+  id: number;
+}
+
+function AvatarList({ max, dataArr }: AvatarProps) {
   return (
     <AvatarGroup max={max}>
       {dataArr?.map(({ nickname, profileImageUrl, id }) => {
